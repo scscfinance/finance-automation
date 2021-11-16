de3ae620c9e05e2c886d6db16adc4ecb0990d437
scscfinance <82142859+scscfinance@users.noreply.github.com>
2021-11-15 20:08:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const test_entry = require('./entry.json');
 const HYPERLINK = (sheet_id, text) => `=HYPERLINK("https://docs.google.com/spreadsheets/d/${sheet_id}", "${text}")`
 
 // Initialize the sheet - doc ID is the long id in the sheets URL
-const annual_budget = new GoogleSpreadsheet('1Mhmwl8KkVu0V7UXxCO4OM6yPfRsGwqA_3sLtEc-OGXM');
+const annual_budget = new GoogleSpreadsheet('1SmJMMxQ0hpVWBpl-dVw1hlJdNc11nNfQfAywEd6LG1c');
 init()
 
 async function init(){
